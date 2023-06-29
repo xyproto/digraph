2a3d6693a6b5e939f0e0f7db1218d46b2812447a
Alexander F. Rødseth <rodseth@gmail.com>
2023-06-29 11:39:24 +0200

    Add tests

--- digraphs_test.go
+++ digraphs_test.go
@@ -1,18 +1,29 @@
 package digraphs
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestLookup(t *testing.T) {
+func TestMustLookup(t *testing.T) {
 	if MustLookup("My") != 'µ' {
 		t.Fail()
 	}
 }
 
-func TestLookupDescription(t *testing.T) {
+func TestLookup(t *testing.T) {
+	if r, ok := Lookup("My"); !ok || r != 'µ' {
+		t.Fail()
+	}
+}
+
+func TestMustLookupDescription(t *testing.T) {
 	if desc := MustLookupDescription("My"); desc != "MICRO SIGN" {
+		t.Fail()
+	}
+}
+
+func TestLookupDescription(t *testing.T) {
+	if desc, ok := LookupDescription("My"); !ok || desc != "MICRO SIGN" {
 		t.Fail()
 	}
 }
